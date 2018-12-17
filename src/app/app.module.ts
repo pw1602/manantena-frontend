30dc2633c6bd18c83769ea080fc21a1c7e385c76
pw1602 <pw1602@gmail.com>
2018-12-17 22:02:53 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RightpanelComponent } from './rightpanel/rightpanel.component';
+import { NewsComponent } from './news/news.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+
+const appRoutes: Routes = [
+	{path: '', redirectTo: '/', pathMatch: 'full'},
+	{path: '', component: NewsComponent}
+]
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		RightpanelComponent,
+		NewsComponent,
+		NavbarComponent,
+		CarouselComponent
+	],
+	imports: [
+		RouterModule.forRoot(
+			appRoutes
+		),
+		BrowserModule,
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
