588fafe4e9fa3c29b14fa157362e83a617d71b81
pw1602 <pw1602@gmail.com>
2019-09-18 13:53:06 +0200

    Poprawa linku api

--- src/app/core/classes/global.ts
+++ src/app/core/classes/global.ts
@@ -1,3 +1,3 @@
 export class Global {
-	public static API_ENDPOINT = "http://localhost:3000/tibia" //'https://pw1602-test-api.glitch.me/tibia';
+	public static API_ENDPOINT = 'https://pw1602-test-api.glitch.me/tibia';
 }
